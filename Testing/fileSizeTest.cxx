89744e804301bb643b3f32822b837dd6e8969103
Robert Maynard <robert.maynard@kitware.com>
2011-12-15 10:58:11 -0500

    Corrected tests to properly pass when run from the build directory.

--- Testing/fileSizeTest.cxx
+++ Testing/fileSizeTest.cxx
@@ -6,8 +6,7 @@ int main(int argc, char **argv)
   const char* fname = "test.txt";
   kwStat ks;
   int ret = kw_stat(fname,&ks);
-
-  assert(ret!=0);
+  assert(ret==0);
 
   //make sure the st_size is a valid number
   assert(ks.st_size==0);
