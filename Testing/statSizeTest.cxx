503c319498ffc3f4a8e4e306551bf996d41d9bcc
Robert Maynard <robert.maynard@kitware.com>
2011-12-15 12:11:56 -0500

    Working on a test to detect we have the correct stat

--- Testing/statSizeTest.cxx
+++ Testing/statSizeTest.cxx
@@ -4,15 +4,25 @@
 
 int main(int argc, char **argv)
 {
-  const char* fname = "test.txt";
   kwStat ks;
 
-  //this needs to be configured to properly
-  //check 32bit vs 64bit
+  //basically this should confirm that we are using the right stat version
+  //based on the off_t size
+  assert(sizeof(off_t)==sizeof(ks.st_size));
 
-  // check that off_t can hold 2^64 - 1
-  off_t temp = (((off_t)1 << 62) + (((off_t)1<<62) -1));
-  assert(temp==(((off_t)1 << 62) + (((off_t)1<<62) -1)));
+  off_t temp;
+  if(sizeof(off_t)==4)
+    {
+    // check that off_t can hold 2^32 - 1
+    temp = (((off_t)1 << 32) + (((off_t)1<<32) -1));
+    assert(temp==(((off_t)1 << 32) + (((off_t)1<<32) -1)));
+    }
+  else
+    {
+    // check that off_t can hold 2^64 - 1
+    temp = (((off_t)1 << 62) + (((off_t)1<<62) -1));
+    assert(temp==(((off_t)1 << 62) + (((off_t)1<<62) -1)));
+    }
 
   // check that the st_size can hold the temp
   ks.st_size = temp;
